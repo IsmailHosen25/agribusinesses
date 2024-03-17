8de7b273ba6ba2af5f9ba2300bb5735f0ec06358
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-03-17 12:30:02 +0600

    login page validation done

--- client/src/Web/Pages/Login/Login.jsx
+++ client/src/Web/Pages/Login/Login.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import style from './Login.module.css'
-import img from "../../../assets/farmer_land.jpg"
+import {useNavigate} from "react-router-dom"
+import {useFormik } from "formik"
+import * as yup from "yup"
 export default function Login() {
+    const navigate =useNavigate()
+    const {values, handleChange,handleBlur,handleSubmit,touched,errors} =useFormik({
+        initialValues:{
+            email:"",
+            password:""
+        },
+        validationSchema:yup.object({
+            email:yup.email,
+            password:yup.string().required("password is required")
+        }),
+        onSubmit:(values)=>{
+            console.log(values)
+            navigate("/")
+        }
+    })
   return (
     <div className={style.login_container}>
     <div className={style.container}>
     <div className={style.wrapper}>
       <div className={style.title}><span>Login Form</span></div>
-      <form action="#" className={style.form}>
+      <form onSubmit={handleSubmit} className={style.form}>
         <div className={style.row}>
           <i className="fas fa-user"></i>
-          <input type="text" placeholder="Email or Phone" className={style.input} required/>
+          <input type="email" placeholder="Email" name='email' value={values.email} onBlur={handleBlur} onChange={handleChange}  className={style.input} required/>
         </div>
         <div className={style.row}>
           <i className="fas fa-lock"></i>
-          <input type="password" placeholder="Password"  className={style.input}  required/>
+          <input type="password" placeholder="Password" name='password' value={values.password} onBlur={handleBlur} onChange={handleChange} className={style.input}  required/>
         </div>
         <div className={style.pass}><a href="#">Forgot password?</a></div>
         <div className={`${style.row} ${style.button}`}>
