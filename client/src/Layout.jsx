d9604765e581b987ceadf98b10cc11c052b41d41
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-03-17 11:42:00 +0600

    login page design done

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Home from './Web/Pages/Home/Home'
+import Login from './Web/Pages/Login/Login'
 export default function Layout() {
   return (
     <>
      <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
      </Routes>
     </>
   )
---
+++ client/src/Web/Pages/Login/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import style from './Login.module.css'
+
+export default function Login() {
+  return (
+    <div className={style.container}>
+    <div className={style.wrapper}>
+      <div className={style.title}><span>Login Form</span></div>
+      <form action="#" className={style.form}>
+        <div className={style.row}>
+          <i className="fas fa-user"></i>
+          <input type="text" placeholder="Email or Phone" className={style.input} required/>
+        </div>
+        <div className={style.row}>
+          <i className="fas fa-lock"></i>
+          <input type="password" placeholder="Password"  className={style.input}  required/>
+        </div>
+        <div className={style.pass}><a href="#">Forgot password?</a></div>
+        <div className={`${style.row} ${style.button}`}>
+          <input type="submit" value="Login"  className={`${style.input} ${style.btninput} ` }/>
+        </div>
+        <div className={style.signup_link}>Not a member? <a href="#">Signup now</a></div>
+      </form>
+    </div>
+  </div>
+
+  )
+}
