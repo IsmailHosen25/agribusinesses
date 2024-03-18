e33c162d9c5a4b5e11ac60ad96f83b04b2b997aa
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-03-18 10:14:00 +0600

    Nav update2

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Home from './Web/Pages/Home/Home'
 import Navbar from './Web/Global Components/Navbar/Navbar'
+import Products from './Web/Pages/Products/Products'
+import About from './Web/Pages/About/About'
+import Contactus from './Web/Pages/Contact us/Contactus'
 export default function Layout() {
   return (
     <>
      <Navbar/>
      <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contactus' element={<Contactus/>}/>
      </Routes>
 
     </>
