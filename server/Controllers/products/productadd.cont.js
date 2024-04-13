2adb31cf1d0452e3f8a2c00decacac9736b61fc8
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-04-13 09:46:15 +0600

    product routing upd 3

--- server/Controllers/products/productadd.cont.js
+++ server/Controllers/products/productadd.cont.js
@@ -1,11 +1,11 @@
 const db=require('../../dbconnection')
 const productadd=(req,res)=>{
    try{
-       const {title,price,code,catagory,number}=req.body
+       const {name,price,quentity,packingdate,expdate,warehouse,number}=req.body
        const {path,filename}=req.file
        db.connect(function(error){
-        const sql="INSERT INTO products(title,filename,filepath,price,code,catagory,number) VALUES( ?, ?, ?, ?, ?, ?, ?)"
-        db.query(sql,[title,filename,path,price,code,catagory,number],async function (error,result){
+        const sql="INSERT INTO products(name,filename,filepath,price,quantity,packingdate,expdate,warehouse,number) VALUES( ?, ?, ?, ?, ?, ?, ?,?,?)"
+        db.query(sql,[name,filename,path,price,quentity,packingdate,expdate,warehouse,number],async function (error,result){
             res.status(200).json({
                 message:"Accepted",
             }) 
